452289e6ce7f144dcf7ba91fe6006c624129f395
lulublue98 <lulu.jiyun.cho@gmail.com>
2017-03-20 18:58:09 -0400

    hello world

--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,13 @@ import requests
 import urllib.request
 import json
 import unicodedata
+
+AppID = '9ce1f47e-ef63-4d4a-8b78-67bebeff4c94'
+
+USERNAME = ''
+PASSWORD = ''
+
+text = 'hello world'
+
+ChangeObject_dict = {}
+
